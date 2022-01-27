8cfd0dfb9c15c71ea429b6a490264940fcd56c33
BladeRED <pokeblade43@gmail.com>
2022-01-27 18:52:24 +0100

    Création et wireframe du projet

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<?php include 'parts/head.php'; ?>
+<body>
+<?php include 'parts/header.php' ?>
+<main>
+
+    <!-- ICI SECTION TITRE -->
+    <section>
+        <div class="container-fluid border title my-5 py-5">
+
+            <h1 class="text-center">TITRE</h1>
+            <h3 class="text-center">"Citation"</h3>
+        </div>
+
+
+        <!-- ICI SECTION DIAPO -->
+    </section>
+    <div class="container border">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://miro.medium.com/max/1200/1*neuubp5DMto1xaR1DZv2Xg.png" class="d-block w-100"
+                         alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://miro.medium.com/max/1200/1*neuubp5DMto1xaR1DZv2Xg.png" class="d-block w-100"
+                         alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://miro.medium.com/max/1200/1*neuubp5DMto1xaR1DZv2Xg.png" class="d-block w-100"
+                         alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                    data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                    data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+
+    </div>
+
+    <!-- ICI SECTION UNIVERS -->
+    <section>
+        <div class="container border">
+            ILLU
+            <a href="#" class="btn btn-outline-light btn-lg text-white">UNIVERS</a>
+
+        </div>
+
+    </section>
+    <!-- ICI SECTION PERSONNAGES -->
+    <section>
+        <div class="container border">
+            ILLU
+            <a href="#" class="btn btn-outline-light btn-lg text-white">PERSONNAGES</a>
+        </div>
+    </section>
+
+    <!-- ICI SECTION BLOC DE TEXTE -->
+    <section>
+
+        <div class="container">
+            <h3 class="text-center text-white">TITRE</h3>
+            <p class="text-white">Nam sed odio vel urna aliquet maximus ut et risus. Aenean sollicitudin massa nec velit
+                malesuada aliquet.
+                Donec porta eleifend condimentum. Proin vitae dui leo. Nulla odio velit, tincidunt eget turpis eu,
+                commodo
+                hendrerit arcu. In hac habitasse platea dictumst. Proin sapien ex, ultrices vel metus quis, congue
+                faucibus
+                neque. Nulla ac euismod diam. Quisque interdum massa vel orci efficitur accumsan. Vivamus molestie
+                tristique
+                felis. In bibendum libero ac pellentesque hendrerit. Aliquam vitae leo quis velit gravida luctus vitae
+                sit
+                amet ligula. Duis tincidunt vehicula lectus, rhoncus luctus tortor mollis sit amet. Praesent tristique,
+                libero quis tempus suscipit, velit nisi ultrices mi, at aliquam risus mauris tincidunt massa.
+
+                Nulla mattis, eros congue ultrices egestas, lorem felis ornare ante, vitae consectetur massa augue eget
+                metus. Integer pulvinar viverra lectus. Phasellus luctus sodales quam nec tempor. Vestibulum lobortis
+                leo
+                eget lacinia egestas. Aenean volutpat lacinia purus, quis sodales ipsum pulvinar non. Curabitur at
+                malesuada
+                ligula. Suspendisse nunc sapien, bibendum eleifend nisi eget, hendrerit bibendum enim. Morbi at cursus
+                est.
+
+                Ut vel ultrices ex. Suspendisse mauris nulla, malesuada ut pretium non, mattis at eros. Suspendisse id
+                nisi
+                nec nibh cursus hendrerit. Nullam malesuada sagittis lectus a congue. Suspendisse potenti. Quisque id
+                sagittis lacus. Phasellus vitae dapibus odio. Nam dapibus porttitor purus. Morbi hendrerit scelerisque
+                nibh,
+                eget gravida nibh. Sed ut mattis purus, sit amet euismod velit. Aenean non lorem tellus. Ut ac faucibus
+                nibh. Quisque mollis nunc et odio rhoncus, eu viverra justo facilisis. Etiam hendrerit sapien dolor, eu
+                pulvinar urna fringilla eu.
+
+                Nam quis nibh velit. Aliquam ut tortor purus. Curabitur risus ex, rutrum nec lobortis at, rhoncus at
+                felis.
+                Suspendisse congue mi id mi posuere, a facilisis elit suscipit. Proin dignissim ullamcorper odio sed
+                ornare.
+                Donec sed ligula iaculis, ultricies nulla nec, vestibulum diam. Cras lacus justo, sollicitudin vitae
+                eleifend vel, pellentesque at ante. Donec ultricies ac nulla sit amet ullamcorper. Nunc pharetra, dui et
+                sodales tincidunt, est leo tempus felis, tincidunt laoreet sapien odio non leo. Pellentesque habitant
+                morbi
+                tristique senectus et netus et malesuada fames ac turpis egestas.
+
+                Aliquam mattis tempor velit, at elementum lacus lobortis id. Nullam dictum ex ut nibh vestibulum, eget
+                lobortis nisl luctus. Integer mauris dui, aliquet interdum nibh vel, auctor ullamcorper massa. Phasellus
+                quis mauris nec leo cursus faucibus ut sit amet leo. Vestibulum ipsum mi, rutrum vitae ante vel,
+                malesuada
+                sollicitudin diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos. Sed sit amet pretium metus.</p>
+        </div>
+    </section>
+
+
+    <!-- ICI SECTION RESEAUX SOCIAUX -->
+    <section>
+        <div class="container mb-5">
+            <h3 class="text-center text-white">RESEAUX SOCIAUX</h3>
+            <div class="row">
+                <a href="#">Logo1</a>
+                <a href="#">Logo2</a>
+                <a href="#">Logo3</a>
+            </div>
+        </div>
+    </section>
+
+
+</main>
+<footer>
+    <!-- ICI MON FOOTER -->
+    <section>
+        <div class="container c-footer">
+            <div class="row">
+                <a href="#">Cookie Notice</a>
+                <a href="#">Terms of Use</a>
+                <a href="#">Privacy Notice</a>
+                <a href="#">Material Usage Policy</a>
+            </div>
+            <div class="row">
+                CREDITS+PEGI
+            </div>
+        </div>
+    </section>
+
+</footer>
+
+</body>
+</html>
---
+++ parts/header.php
@@ -0,0 +1,13 @@
+<!-- ICI MA NAVBAR -->
+
+<section>
+    <nav class="navbar navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24"
+                     class="d-inline-block align-text-top">
+                Bootstrap
+            </a>
+        </div>
+    </nav>
+</section>
